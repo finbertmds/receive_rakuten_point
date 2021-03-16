b5ae65417c3cb4be1906dc697edad71c54fc4649
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2021-03-17 07:08:42 +0900

    increase timeout

--- wdio.shared.conf.js
+++ wdio.shared.conf.js
@@ -138,7 +138,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 900000
+        timeout: 1800000
     },
     //
     // =====
