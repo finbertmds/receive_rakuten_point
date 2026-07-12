7c9f5515fd6d3181d14c0748b51bbf32890e8a46
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2026-07-12 11:37:05 +0900

    fix: comment out unused test case for visiting mission page

--- test/specs/infoseek.point.e2e.ts
+++ test/specs/infoseek.point.e2e.ts
@@ -50,13 +50,13 @@ describe('Infoseek', () => {
         await infoseekPage.readArticleAtRankingPage(rankingPage2);
     });
 
-    it('infoseek__visit_mission', async () => {
-        await credentials();
-        await browser.pause(2000);
+    // it('infoseek__visit_mission', async () => {
+    //     await credentials();
+    //     await browser.pause(2000);
 
-        await infoseekPage.closeModalContents();
-        await infoseekPage.visitMissionPage();
-    });
+    //     await infoseekPage.closeModalContents();
+    //     await infoseekPage.visitMissionPage();
+    // });
 
     it('infoseek__get_point', async () => {
         await credentials();
